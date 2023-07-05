e43814632bef3f6aedf0cc8c8471be5aca6653e3
furkanisik000 <138674899+furkanisik000@users.noreply.github.com>
2023-07-05 14:07:23 +0300

    Update class.php
    ses ve fotoğraf ve dosya gönderimi eklendi

--- class.php
+++ class.php
@@ -32,6 +32,44 @@ class TelegramBot {
         $this->sendRequest('editMessageText', $data);
     }
 
+    public function sendPhoto($chatId, $photo, $options = array()) {
+        $data = array(
+            'chat_id' => $chatId,
+            'photo' => $photo
+        );
+
+        $data = array_merge($data, $options);
+
+        $this->sendRequest('sendPhoto', $data);
+    }
+
+    public function sendAudio($chatId, $audio, $options = array()) {
+        $data = array(
+            'chat_id' => $chatId,
+            'audio' => $audio
+        );
+
+        $data = array_merge($data, $options);
+
+        $this->sendRequest('sendAudio', $data);
+    }
+
+    public function sendDocument($chatId, $document, $options = array()) {
+        $data = array(
+            'chat_id' => $chatId,
+            'document' => $document
+        );
+
+        $data = array_merge($data, $options);
+
+        $this->sendRequest('sendDocument', $data);
+    }
+
+
+    // Diğer API özelliklerini buraya ekleyebilirsiniz
+
+    // ...
+
     public function sendRequest($method, $data = array()) {
         $url = $this->apiUrl . $method;
         $options = array(
@@ -53,10 +91,20 @@ class TelegramBot {
 $botToken = 'your_bot_token';
 $telegramBot = new TelegramBot($botToken);
 
+// Örnek mesaj gönderimi
 $chatId = 'your_chat_id';
 $message = 'Merhaba, Dünya!';
 $telegramBot->sendMessage($chatId, $message);
 
+// Mesaj düzenleme
 $messageId = 'your_message_id';
 $newMessage = 'Yeni mesaj metni';
 $telegramBot->editMessageText($chatId, $messageId, $newMessage);
+
+// Build komutu gönderme
+$projectName = 'my_project';
+$version = '1.0.0';
+$telegramBot->sendBuildCommand($chatId, $projectName, $version);
+
+// Diğer API özelliklerini kullanmak için sınıfın ilgili metodlarını çağırabilirsiniz
+// Örneğin: $telegramBot->sendPhoto(), $telegramBot->sendAudio(), vb.
